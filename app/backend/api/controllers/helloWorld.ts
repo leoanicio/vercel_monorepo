d49d44d53464bb4b6a7f7d0c90c4a7225953c979
Leonardo Anicio <leonardoaapereira@gmail.com>
2023-12-08 15:32:18 -0300

    teste

--- app/backend/api/controllers/helloWorld.ts
+++ app/backend/api/controllers/helloWorld.ts
@@ -1,11 +1,15 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
  
-export default function handler(
+
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse,
 ) {
+  const users = await prisma.users.findMany()
   response.status(200).json({
-    body: request.body,
+    body: users,
     query: request.query,
     cookies: request.cookies,
   });
